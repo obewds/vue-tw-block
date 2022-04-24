380fd09f336764ec42208016c775f2e8103447e9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-04-23 23:13:07 -0400

    Initial commit

---
+++ src/app/Workspace.vue
@@ -0,0 +1,27 @@
+<!--
+    ./src/app/Workspace.vue
+-->
+
+
+<script setup lang="ts">
+
+    // This starter template is using Vue 3 <script setup> SFCs
+    // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+    import ViteVueTsCompoPkgStarter from '@/components/ViteVueTsCompoPkgStarter.vue'
+    //import { ViteVueTsCompoPkgStarter } from 'vite-vue-ts-compo-pkg-starter'
+
+</script>
+
+
+<template>
+
+    <div class="text-green-500 text-xs pb-1">WORKSPACE</div>
+    
+    <div class="bg-white text-center border-t-2 border-b-2 border-green-500 border-dashed py-24">
+
+        <ViteVueTsCompoPkgStarter/>
+
+    </div>
+
+</template>
